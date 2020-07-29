7c2b4fc07e394810c802ac736ea27a2ebc58836d
TotallyGamerJet <totallygamerjet@gmail.com>
2020-07-28 21:34:35 -0400

    Update nsgeometry.go
    new functions

--- foundation/nsgeometry.go
+++ foundation/nsgeometry.go
@@ -1,8 +1,15 @@
 package foundation
 
+import "fmt"
+
 /*
 #import <Foundation/NSGeometry.h>
 */
+
+var (
+	//An NSPoint structure with both x and y coordinates set to 0.
+	NSZeroPoint = NSPoint{}
+)
 
 //TODO: NSPoint methods
 type NSPoint struct {
@@ -10,7 +17,16 @@ type NSPoint struct {
 }
 
 func NSMakePoint(x, y float64) NSPoint {
-	return NSPoint{x, y} //Point: C.NSMakePoint(C.double(x), C.double(y))}
+	return NSPoint{x, y}
+}
+
+//Returns a Boolean value that indicates whether two points are equal.
+func NSEqualPoint(aPoint, bPoint NSPoint) bool {
+	return aPoint.x == bPoint.x && aPoint.y == bPoint.y
+}
+
+func (point NSPoint) String() string {
+	return fmt.Sprintf("{%f,%f}", point.x, point.y)
 }
 
 //TODO: NSRect methods
@@ -19,5 +35,5 @@ type NSRect struct {
 }
 
 func NSMakeRect(x, y, width, height float64) NSRect {
-	return NSRect{x, y, width, height} //Rect: C.NSMakeRect(C.double(x), C.double(y), C.double(width), C.double(height))}
+	return NSRect{x, y, width, height}
 }
